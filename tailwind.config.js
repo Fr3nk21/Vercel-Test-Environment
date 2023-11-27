43687fdce499f40bb926ba7385db1c5431aadecd
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2023-11-27 12:01:27 +1100

    Landing, Three column & Photo right/left sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '767px',
+      lg: '979px',
+      xl: '1200px',
+    },
+    // spacing: {
+
+    // }
+    // colors: {
+
+    // },
+    // fontFamily: {
+
+    // },
     extend: {},
   },
   plugins: [],
